df84cfdc6f965f1117f6ecfa02bdc5f97f28bfb0
ZenKhalil <feattzen@gmail.com>
2023-09-06 19:11:51 +0200

    Images for Artists added

--- app.js
+++ app.js
@@ -1,12 +1,155 @@
+"use strict";
+
+let artists = [];
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
 document.addEventListener('DOMContentLoaded', () => {
+    fetchArtists();
+});
+
+function fetchArtists() {
     fetch('http://localhost:3000/artists')
-    .then(response => response.json())
-    .then(data => {
-        const list = document.getElementById('artists-list');
-        data.forEach(artist => {
-            const listItem = document.createElement('li');
-            listItem.textContent = artist.name;
-            list.appendChild(listItem);
+        .then(response => response.json())
+        .then(data => {
+            artists = data;
+            // Check current URL to display the correct content
+            const currentPath = window.location.pathname;
+            if (currentPath === '/artists' || currentPath === '/') {
+                showArtists();
+            } else if (currentPath === '/genre') {
+                showGenre();
+            } else if (currentPath === '/favorites') {
+                showFavorites();
+            } else if (currentPath === '/about') {
+                showAbout();
+            } else {
+                showHome();
+            }
+            
+            document.getElementById('home-link').addEventListener('click', showHome);
+            document.getElementById('artists-link').addEventListener('click', showArtists);
+            document.getElementById('genre-link').addEventListener('click', showGenre);
+            document.getElementById('favorites-link').addEventListener('click', showFavorites);
+            document.getElementById('about-link').addEventListener('click', showAbout);
+        });
+}
+
+window.addEventListener('popstate', (event) => {
+    if (event.state) {
+        switch (event.state.view) {
+            case 'home':
+                showHome();
+                break;
+            case 'artists':
+                showArtists();
+                break;
+            case 'genre':
+                showGenre();
+                break;
+            case 'favorites':
+                showFavorites();
+                break;
+            case 'about':
+                showAbout();
+                break;
+        }
+    }
+});
+
+
+const contentDiv = document.getElementById('content');
+
+function showHome() {
+    contentDiv.innerHTML = `<h1>Welcome to Artist Directory</h1>
+                            <p>Explore the world of music artists!</p>`;
+    history.pushState({ view: 'home' }, '', '/home');
+}
+
+function showArtists() {
+    let artistListHTML = '<ul>';
+    artists.forEach(artist => {
+        artistListHTML += `
+        <li>
+            <h2>${artist.name}</h2>
+            <img src="${artist.image}" alt="${artist.name}" width="100" />
+            <p>${artist.shortDescription}</p>
+            <a href="${artist.website}">Website</a>
+            <button onclick="toggleFavorite(${artist.id})">${favorites.includes(artist.id) ? 'Remove from Favorites' : 'Add to Favorites'}</button>
+        </li>`;
+    });
+    artistListHTML += '</ul>';
+    contentDiv.innerHTML = artistListHTML;
+   history.pushState({ view: 'artists' }, '', '/artists');
+}
+
+function getUniqueGenres() {
+    const genres = new Set();
+    artists.forEach(artist => {
+        artist.genres.forEach(genre => {
+            genres.add(genre);
         });
     });
-});
+    return [...genres];
+}
+
+function showGenre() {
+    const uniqueGenres = getUniqueGenres();
+    let genreListHTML = '<ul>';
+    uniqueGenres.forEach(genre => {
+        genreListHTML += `<li><a href="#" onclick="showArtistsByGenre('${genre}')">${genre}</a></li>`;
+    });
+    genreListHTML += '</ul>';
+    contentDiv.innerHTML = genreListHTML;
+ history.pushState({ view: 'genre' }, '', '/genre');
+}
+
+function showArtistsByGenre(genre) {
+    const filteredArtists = artists.filter(artist => artist.genres.includes(genre));
+    let artistListHTML = `<h1>${genre}</h1><ul>`;
+    filteredArtists.forEach(artist => {
+        artistListHTML += `
+        <li>
+            <h2>${artist.name}</h2>
+            <img src="${artist.image}" alt="${artist.name}" width="100" />
+            <p>${artist.shortDescription}</p>
+            <a href="${artist.website}">Website</a>
+            <button onclick="toggleFavorite(${artist.id})">${favorites.includes(artist.id) ? 'Remove from Favorites' : 'Add to Favorites'}</button>
+        </li>`;
+    });
+    artistListHTML += '</ul>';
+    contentDiv.innerHTML = artistListHTML;
+}
+
+function showFavorites() {
+    let favoritesListHTML = '<h1>Favorites</h1><ul>';
+    const favoriteArtists = artists.filter(artist => favorites.includes(artist.id));
+    favoriteArtists.forEach(artist => {
+        favoritesListHTML += `
+        <li>
+            <h2>${artist.name}</h2>
+            <img src="${artist.image}" alt="${artist.name}" width="100" />
+            <p>${artist.shortDescription}</p>
+            <a href="${artist.website}">Website</a>
+            <button onclick="toggleFavorite(${artist.id})">Remove from Favorites</button>
+        </li>`;
+    });
+    favoritesListHTML += '</ul>';
+    contentDiv.innerHTML = favoritesListHTML;
+  history.pushState({ view: 'favorites' }, '', '/favorites');
+}
+
+function toggleFavorite(artistId) {
+    if (favorites.includes(artistId)) {
+        const index = favorites.indexOf(artistId);
+        favorites.splice(index, 1);
+    } else {
+        favorites.push(artistId);
+    }
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    showArtists();  // Opdaterer kunstnerlisten efter ændringer i favoritter
+}
+
+function showAbout() {
+    contentDiv.innerHTML = "<h1>About Us</h1><p>We are a platform dedicated to showcasing music artists.</p>";
+    history.pushState({ view: 'about' }, '', '/about');
+}
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-
-
-const express = require('express');
-const cors = require('cors');
-const artists = require('./artists.json');
+import express from 'express';
+import cors from 'cors';
+import artists from './artists.json' assert { type: "json" };
+import loginRouter from './login.js';
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json()); // Dette gør, at vi kan læse JSON fra POST-anmodninger
+app.use('/admin-login', loginRouter);
 
+// 1. Læs alle kunstnere
 app.get('/artists', (req, res) => {
     res.json(artists);
 });
 
+// 2. Læs en specifik kunstner baseret på ID
 app.get('/artists/:id', (req, res) => {
     const artistId = parseInt(req.params.id, 10);
     const artist = artists.find(a => a.id === artistId);
@@ -23,6 +26,42 @@ app.get('/artists/:id', (req, res) => {
     }
 });
 
+// 3. Opret en ny kunstner
+app.post('/artists', (req, res) => {
+    const newArtist = req.body;
+    newArtist.id = Math.max(...artists.map(a => a.id)) + 1; // Tilføjer et nyt unikt ID
+    artists.push(newArtist);
+    res.status(201).json(newArtist);
+});
+
+// 4. Opdater en kunstner
+app.put('/artists/:id', (req, res) => {
+    const artistId = parseInt(req.params.id, 10);
+    const updatedArtist = req.body;
+    const artistIndex = artists.findIndex(a => a.id === artistId);
+    
+    if (artistIndex !== -1) {
+        artists[artistIndex] = updatedArtist;
+        res.json(updatedArtist);
+    } else {
+        res.status(404).json({ error: "Artist not found" });
+    }
+});
+
+// 5. Slet en kunstner
+app.delete('/artists/:id', (req, res) => {
+    const artistId = parseInt(req.params.id, 10);
+    const artistIndex = artists.findIndex(a => a.id === artistId);
+    
+    if (artistIndex !== -1) {
+        artists.splice(artistIndex, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).json({ error: "Artist not found" });
+    }
+});
+
+// Start serveren
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
