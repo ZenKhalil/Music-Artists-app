24948325aef207df0fa3ad0c349006128b71e74d
ZenKhalil <feattzen@gmail.com>
2023-09-08 15:03:14 +0200

    Fix the search live function

--- search.js
+++ search.js
@@ -2,30 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let searchBar = document.getElementById("search-bar");
 
-    // Search functions
     function performSearch() {
         const query = searchBar.value.toLowerCase();
     
-        const filteredArtists = window.artists.filter(artist => 
-            artist.name.toLowerCase().includes(query)
-        );
-    
-        displayFilteredArtists(filteredArtists);
-    }
-
-    function displayFilteredArtists(filteredArtists) {
-        let artistHTML = '';
-        filteredArtists.forEach(artist => {
-            artistHTML += `
-                <div class="artist-card">
-                    <img src="images/${artist.image}" alt="${artist.name}">
-                    <h3>${artist.name}</h3>
-                    <p>${artist.shortDescription}</p>
-                    <a href="${artist.website}" target="_blank">Visit Website</a>
-                    <button onclick="toggleFavorite(${artist.id})">${window.favorites.includes(artist.id) ? 'Remove from Favorites' : 'Add to Favorites'}</button>
-                </div>`;
+        const artistCards = document.querySelectorAll('.artist-card'); // select all artist cards on the page
+        artistCards.forEach(card => {
+            const artistName = card.querySelector('h3').textContent.toLowerCase(); // get the artist name from the card
+            if (artistName.includes(query)) {
+                card.style.display = 'block'; // show card if name matches query
+            } else {
+                card.style.display = 'none'; // hide card if name doesn't match query
+            }
         });
-        document.getElementById('content').innerHTML = artistHTML;
     }
 
     // Event listeners
@@ -42,3 +30,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+    function displayFilteredArtists(filteredArtists) {
+        let artistHTML = '';
+        filteredArtists.forEach(artist => {
+            artistHTML += `
+                <div class="artist-card">
+                    <img src="images/${artist.image}" alt="${artist.name}">
+                    <h3>${artist.name}</h3>
+                    <p>${artist.shortDescription}</p>
+                    <a href="${artist.website}" target="_blank">Visit Website</a>
+                    <button onclick="toggleFavorite(${artist.id})">${window.favorites.includes(artist.id) ? 'Remove from Favorites' : 'Add to Favorites'}</button>
+                </div>`;
+        });
+        document.getElementById('content').innerHTML = artistHTML;
+    }
